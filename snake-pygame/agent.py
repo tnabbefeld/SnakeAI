bf2cabdf6b2f2a0ba8b7a304801055eba5ebb488
Travis Nabbefeld <tnabbefeld@gmail.com>
2022-05-25 23:41:40 -0500

    Final Implementation

--- snake-pygame/agent.py
+++ snake-pygame/agent.py
@@ -3,20 +3,22 @@ import random
 import numpy as np
 from collections import deque
 from game import SnakeGameAI, Direction, Point
+from model import Linear_QNet, QTrainer
+from helper import plot
 
 MAX_MEMORY = 100_000
 BATCH_SIZE = 1000
 LR = 0.001
 
 class Agent:
+    
     def __init__(self):
         self.n_games = 0
         self.epsilon = 0 # randomness
-        self.gamma = 0 # discount rate
+        self.gamma = 0.9 # discount rate
         self.memory = deque(maxlen=MAX_MEMORY) # popleft()
-        self.model = None # TODO
-        self.trainer = None # TODO
-        # TODO: model, trainer
+        self.model = Linear_QNet(11, 256, 3)
+        self.trainer = QTrainer(self.model, lr=LR, gamma=self.gamma)
 
     def get_state(self, game):
         head = game.snake[0]
@@ -88,49 +90,53 @@ class Agent:
             final_move[move] = 1
         else:
             state0 = torch.tensor(state, dtype=torch.float)
-            prediction = self.model.predict(state0)
+            prediction = self.model(state0)
             move = torch.argmax(prediction).item()
             final_move[move] = 1
 
         return final_move
 
-    def train():
-        plot_scores = []
-        plot_mean_scores = []
-        total_score = 0
-        record = 0
-        agent = Agent()
-        game = SnakeGameAI()
-        while True:
-            # get old state
-            state_old = agent.get_state(game)
+def train():
+    plot_scores = []
+    plot_mean_scores = []
+    total_score = 0
+    record = 0
+    agent = Agent()
+    game = SnakeGameAI()
+    while True:
+        # get old state
+        state_old = agent.get_state(game)
 
-            # get move
-            final_move = agent.get_action(state_old)
+        # get move
+        final_move = agent.get_action(state_old)
 
-            # perform move and get new state
-            reward, done, score = game.play_step(final_move)
-            state_new = agent.get_state(game)
+        # perform move and get new state
+        reward, done, score = game.play_step(final_move)
+        state_new = agent.get_state(game)
 
-            # train short memory
-            agent.train_short_memory(state_old, final_move, reward, state_new, done)
+        # train short memory
+        agent.train_short_memory(state_old, final_move, reward, state_new, done)
 
-            # remember
-            agent.remember(state_old, final_move, reward, state_new, done)
+        # remember
+        agent.remember(state_old, final_move, reward, state_new, done)
 
-            if done:
-                # train long memory, plot result
-                game.reset()
-                agent.n_games += 1
-                agent.train_long_memory()
+        if done:
+            # train long memory, plot result
+            game.reset()
+            agent.n_games += 1
+            agent.train_long_memory()
 
-                if score > record:
-                    record = score
-                    # agent.model.save()
+            if score > record:
+                record = score
+                agent.model.save()
 
-                print('Game', agent.n_games, 'Score', score, 'Record:', record)
+            print('Game', agent.n_games, 'Score', score, 'Record:', record)
 
-                # TODO: plot
+            plot_scores.append(score)
+            total_score += score
+            mean_score = total_score / agent.n_games
+            plot_mean_scores.append(mean_score)
+            plot(plot_scores, plot_mean_scores)
 
-    if __name__ == '__main__':
-        train()
+if __name__ == '__main__':
+    train()
